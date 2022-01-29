5c6c57dcd49aa701ef73e796f09063ef73da28a6
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2022-01-29 14:39:59 -0300

    app

--- fusion/settings.py
+++ fusion/settings.py
@@ -35,8 +35,8 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'core',
 
-    'django adminlte',
-    'django adminlte theme',
+    'django_adminlte',
+    'django_adminlte theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
