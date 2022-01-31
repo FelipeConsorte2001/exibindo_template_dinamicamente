9d589cd997afe04a0af5469daec7393d065b0bf0
FelipeConsorte2001 <cfelipeconsorte2001@gmail.com>
2022-01-31 09:36:29 -0300

    rota

--- fusion/settings.py
+++ fusion/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = 'django-insecure-x4rpm_4c5=i0=86$6jlx1i15_k8)_v!m6phd^av+a7q*p9h*v9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
 ALLOWED_HOSTS = ['*']
 
 
@@ -133,6 +134,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
